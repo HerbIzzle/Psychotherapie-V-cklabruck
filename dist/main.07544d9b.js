79f3392a7b94350f6991978ee36eb2e074fd23a5
Herbert Knaf <58120773+HerbIzzle@users.noreply.github.com>
2022-05-17 16:59:42 +0200

    minor fixes

--- dist/main.07544d9b.js
+++ dist/main.07544d9b.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"C:\\Users\\asus\\Desktop\\Psychotherapie Vöcklabruck\\img\\header-big-foto.jpg":[["header-big-foto.da5aada4.jpg","img/header-big-foto.jpg"],"img/header-big-foto.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"C:\\Users\\asus\\Desktop\\Psychotherapie Vöcklabruck\\img\\leather-couch.jpg":[["leather-couch.5eb0bbf4.jpg","img/leather-couch.jpg"],"img/leather-couch.jpg"],"C:\\Users\\asus\\Desktop\\Psychotherapie Vöcklabruck\\img\\header-big-foto.jpg":[["header-big-foto.da5aada4.jpg","img/header-big-foto.jpg"],"img/header-big-foto.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53336" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54518" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
